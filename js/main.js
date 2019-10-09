dd295ff33122365b16029e5cf6c5d570a5d88001
Rimantas <belauzas@yahoo.com>
2019-10-09 20:44:05 +0300

    Grid system

--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
 "use strict";
 
+let currentPage = 'list';
+
+function renderHeader() {
+    let HTML = '';
+    let registerHTML = '<div class="link">Login</div>';
+    let loginHTML = '<div class="link">Register</div>';
+    let accountHTML = `<div class="user-img"></div>
+                        <div class="user-name">Vardenis Pavardenis</div>
+                        <div class="link">Logout</div>`;
+    let partial = '';
+
+    switch (currentPage) {
+        case 'register':
+            partial = registerHTML;
+            break;
+
+        case 'login':
+            partial = loginHTML;
+            break;
+
+        case 'list':
+        case 'item':
+            partial = accountHTML;
+            break;
+    
+        default:
+            break;
+    }
+
+    HTML = `<div class="logo">TODO</div>
+            <div class="right">
+                ${partial}
+            </div>`;
+
+    return document.querySelector('header').innerHTML = HTML;
+}
+
+renderHeader();
